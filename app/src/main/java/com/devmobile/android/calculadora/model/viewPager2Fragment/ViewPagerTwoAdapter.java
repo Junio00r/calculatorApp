bce6afd4699235a2376613b2487a96a9aafcc48f
Junior Oliveira <jo2025698@gmail.com>
2023-10-26 13:15:10 -0300

    att

---
+++ app/src/main/java/com/devmobile/android/calculadora/model/viewPager2Fragment/ViewPagerTwoAdapter.java
@@ -0,0 +1,23 @@
+package com.devmobile.android.calculadora.model.viewPager2Fragment;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerTwoAdapter extends FragmentStateAdapter {
+    public ViewPagerTwoAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
